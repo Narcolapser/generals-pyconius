502477af9f02f71a0c7f8b3848ce6571654f2302
Toby Archer <Toben.Archer@CovenantEyes.com>
2022-08-11 16:02:37 -0500

    Generals now can run a simple bot, and map printing.
    The map printing has clued me in to what appears to be a miss match in
    how the game views the world and how this code base does.

--- generalsio.py
+++ generalsio.py
@@ -1,5 +1,7 @@
 import json
 import math
+import random
+from string import ascii_letters
 from socketIO_client import SocketIO, BaseNamespace
 
 
@@ -236,3 +238,108 @@ def _patch(old, diff):
 
 def pretty_print(json_like):
     print(json.dumps(json_like, sort_keys=True, indent=4, separators=(',', ': ')))
+
+map_map = {-1:' ',-2:'M',-3:'.',-4:'m'}
+
+def print_map(map):
+    for i, row in enumerate(map):
+        for char in row:
+            if char < 0:
+                print(map_map[char],end='')
+            else:
+                print(char,end='')
+        print()
+    print()
+        
+
+class Tile(object): # enum
+    EMPTY = -1
+    MOUNTAIN = -2
+    UNKNOWN = -3
+    UNKNOWN_OBSTACLE = -4
+
+class WorldUnderstanding(object):
+    def __init__(self):
+        self.map_size = (None, None)
+        self.enemy_pos = None
+        self.player_pos = None
+
+        self.cities = None
+        self.mountains = set()
+
+        self.last_seen_scores = None
+        self.expected_scores = None
+
+    def update(self, tiles, armies, cities, enemy_position, enemy_total_army, enemy_total_land):
+        for x in range(self.map_size[0]):
+            for y in range(self.map_size[1]):
+                    if tiles[x][y] == Tile.MOUNTAIN:
+                        self.mountains.add((x, y))
+
+
+class Bot(GameClientListener):
+    def __init__(self, user_id, username, custom_game_name):
+        print('building bot')
+        self.client = GameClient(user_id, username)
+        self.world = WorldUnderstanding()
+        self.client.add_listener(self)
+        self.game_over = False
+        print('instatiated')
+        if custom_game_name:
+            self.client.join_custom(custom_game_name)
+        else:
+            self.client.join_1v1_queue()
+
+    def handle_game_update(self, half_turns, tiles, armies, cities, enemy_position, 
+                           enemy_total_army, enemy_total_land):
+        self.world.update(tiles, armies, cities, enemy_position, enemy_total_army, enemy_total_land)
+        print_map(tiles)
+
+        # Move randomly
+        delta_moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        #moves = [np.add(self.world.player_pos, delta_move) for delta_move in delta_moves]
+        moves = [(self.world.player_pos[0] + move[0], self.world.player_pos[1] + move[1]) for move in delta_moves]
+        def move_feasible(position):
+            if tuple(position) in self.world.mountains:
+                return False
+            if not 0 <= position[0] < self.world.map_size[0]:
+                return False
+            if not 0 <= position[1] < self.world.map_size[1]:
+                return False
+            return True
+        move_options = [move for move in moves if move_feasible(move)]
+        move = random.choice(move_options)
+        self.client.attack(self.world.player_pos, move)
+
+    def handle_game_start(self, map_size, start_pos, enemy_username):
+        print('Starting game')
+        self.world.map_size = map_size
+        self.world.player_pos = start_pos
+
+    def handle_game_over(self, won, replay_url):
+        if won:
+            header = 'Game Won'
+        else:
+            header = 'Game Lost'
+        print(header)
+        print('='*len(header))
+        print('Replay: %s\n' % replay_url)
+        self.game_over = True
+
+    def handle_chat(self, username, message):
+        print('%s: %s' % (username, message))
+
+    def block_forever(self):
+        while not self.game_over:
+            self.client.wait(seconds=2)
+
+if __name__ == '__main__':
+    userid = ''.join([random.choice(ascii_letters) for i in range(16)])
+    username = '[Bot] ' + ''.join([random.choice(ascii_letters) for i in range(4)])
+    custom_game_name = "narconium"
+    run_forever = False
+    while True:
+        bot = Bot(userid, username, custom_game_name)
+        bot.block_forever()
+        if not run_forever:
+            break
